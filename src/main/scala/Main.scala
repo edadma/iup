7934ffa5436b0144a95234220d7ff291daf7fbd7
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-19 08:58:03 -0400

    example 4

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -36,25 +36,55 @@ import scala.scalanative.unsafe.{CFuncPtr1, CInt, CQuote, Zone, toCString}
 //
 //}
 
+//object Main extends App {
+//
+//  val btn_exit_cb = (h: Ihandle) => {
+//    IupMessage("Hello World Message", "Hello world from IUP.")
+//
+//    /* Exits the main loop */
+//    IUP_CLOSE
+//  }
+//
+//  if (IupOpen == IUP_ERROR) {
+//    println("Error opening window")
+//    sys.exit(1)
+//  }
+//
+//  val button = IupButton("OK", null);
+//  val vbox   = IupVbox(button);
+//  val dlg    = IupDialog(IupVbox(vbox))
+//
+//  dlg.TITLE = "Hello World 3"
+//
+//  /* Registers callbacks */
+//  button.ACTION = btn_exit_cb
+//
+//  dlg.IupShowXY(IUP_CENTER, IUP_CENTER)
+//
+//  IupMainLoop
+//
+//  IupClose()
+//
+//}
+
 object Main extends App {
 
-  val btn_exit_cb = (h: Ihandle) => {
-    IupMessage("Hello World Message", "Hello world from IUP.")
-
-    /* Exits the main loop */
+  val btn_exit_cb =
+    (h: Ihandle) =>
+      /* Exits the main loop */
     IUP_CLOSE
-  }
 
   if (IupOpen == IUP_ERROR) {
     println("Error opening window")
     sys.exit(1)
   }
 
-  val button = IupButton("OK", null);
-  val vbox   = IupVbox(button);
-  val dlg    = IupDialog(IupVbox(vbox))
+  val label  = IupLabel("Hello world from IUP.")
+  val button = IupButton("OK", null)
+  val vbox   = IupVbox(label, button)
+  val dlg    = IupDialog(vbox)
 
-  dlg.TITLE = "Hello World 3"
+  dlg.TITLE = "Hello World 4"
 
   /* Registers callbacks */
   button.ACTION = btn_exit_cb
