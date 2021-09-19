6a64455af2066d2e2ebc237b1c79c590322abfac
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-19 11:25:02 -0400

    example 5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -95,20 +95,20 @@ import io.github.edadma.iup._
 
 object Main extends App {
 
-  val buttonExitCallback = (h: Ihandle) => IUP_CLOSE
+  val buttonExitCallback = (h: Handle) => Return.CLOSE
 
-  if (IupOpen == IUP_ERROR) {
+  if (open == Result.ERROR) {
     println("Error opening window")
     sys.exit(1)
   }
 
-  val label  = IupLabel("Hello world from IUP.")
-  val button = IupButton("OK", null)(action = buttonExitCallback)
-  val vbox   = IupVbox(label, button)(alignment = "acenter", gap = 10, margin = 30 x 10)
-  val dlg    = IupDialog(vbox)(title = "Hello World 5")
+  val lbl = label("Hello world from IUP.")
+  val btn = button("OK", null)(action = buttonExitCallback)
+  val vbx = vbox(lbl, btn)(alignment = "acenter", gap = 10, margin = 30 x 10)
+  val dlg = dialog(vbx)(title = "Hello World 5")
 
-  dlg.IupShowXY(IUP_CENTER, IUP_CENTER)
-  IupMainLoop
-  IupClose()
+  dlg.showXY(Position.CENTER, Position.CENTER)
+  mainLoop
+  close()
 
 }
--- src/main/scala/io/github/edadma/iup/package.scala
+++ src/main/scala/io/github/edadma/iup/package.scala
@@ -10,47 +10,50 @@ import io.github.edadma.iup.extern.{LibIUP => lib}
 package object iup {
 
   // Common Flags and Return Values
-  implicit class IupResult(val res: CInt) extends AnyVal
-
-  lazy val IUP_ERROR: IupResult   = IupResult(1)
-  lazy val IUP_NOERROR: IupResult = IupResult(0)
-  lazy val IUP_INVALID: IupResult = IupResult(-1)
+  implicit class Result(val res: CInt) extends AnyVal
+  object Result {
+    final val ERROR: Result   = Result(1)
+    final val NOERROR: Result = Result(0)
+    final val INVALID: Result = Result(-1)
+  }
 
   lazy val IUP_OPENED: Int = -1
 
   // Callback Return Values
 
   // IupPopup and IupShowXY Parameter Values
-  implicit class IupPosition(val pos: CInt) extends AnyVal
-
-  lazy val IUP_CENTER: IupPosition       = IupPosition(0xFFFF) /* 65535 */
-  lazy val IUP_LEFT: IupPosition         = IupPosition(0xFFFE) /* 65534 */
-  lazy val IUP_RIGHT: IupPosition        = IupPosition(0xFFFD) /* 65533 */
-  lazy val IUP_MOUSEPOS: IupPosition     = IupPosition(0xFFFC) /* 65532 */
-  lazy val IUP_CURRENT: IupPosition      = IupPosition(0xFFFB) /* 65531 */
-  lazy val IUP_CENTERPARENT: IupPosition = IupPosition(0xFFFA) /* 65530 */
-  lazy val IUP_LEFTPARENT: IupPosition   = IupPosition(0xFFF9) /* 65529 */
-  lazy val IUP_RIGHTPARENT: IupPosition  = IupPosition(0xFFF8) /* 65528 */
-  lazy val IUP_TOP: IupPosition          = IUP_LEFT
-  lazy val IUP_BOTTOM: IupPosition       = IUP_RIGHT
-  lazy val IUP_TOPPARENT: IupPosition    = IUP_LEFTPARENT
-  lazy val IUP_BOTTOMPARENT: IupPosition = IUP_RIGHTPARENT
+  implicit class Position(val pos: CInt) extends AnyVal
+  object Position {
+    final val CENTER: Position       = Position(0xFFFF) /* 65535 */
+    final val LEFT: Position         = Position(0xFFFE) /* 65534 */
+    final val RIGHT: Position        = Position(0xFFFD) /* 65533 */
+    final val MOUSEPOS: Position     = Position(0xFFFC) /* 65532 */
+    final val CURRENT: Position      = Position(0xFFFB) /* 65531 */
+    final val CENTERPARENT: Position = Position(0xFFFA) /* 65530 */
+    final val LEFTPARENT: Position   = Position(0xFFF9) /* 65529 */
+    final val RIGHTPARENT: Position  = Position(0xFFF8) /* 65528 */
+    final val TOP: Position          = LEFT
+    final val BOTTOM: Position       = RIGHT
+    final val TOPPARENT: Position    = LEFTPARENT
+    final val BOTTOMPARENT: Position = RIGHTPARENT
+  }
 
   // Callback Return Values
-  implicit class IupReturn(val value: CInt) extends AnyVal
-
-  lazy val IUP_IGNORE: IupReturn   = IupReturn(-1)
-  lazy val IUP_DEFAULT: IupReturn  = IupReturn(-2)
-  lazy val IUP_CLOSE: IupReturn    = IupReturn(-3)
-  lazy val IUP_CONTINUE: IupReturn = IupReturn(-4)
+  implicit class Return(val value: CInt) extends AnyVal
+  object Return {
+    final val IGNORE: Return   = Return(-1)
+    final val DEFAULT: Return  = Return(-2)
+    final val CLOSE: Return    = Return(-3)
+    final val CONTINUE: Return = Return(-4)
+  }
 
   // atom allocation Zone is for 'const char*' string arguments
   //   it was discovered that, in this library, strings that are declared 'const char*' can't be freed, at least
   //   until the element is displayed.  So, to be safe, they are never free (but also never duplicated) until
   //   the UI is closed
   // only for UI attributes and text which change seldomly if ever; gets closed by IupClose()
-  private lazy implicit val atomZone: Zone = Zone.open()
-  private lazy val atoms                   = new mutable.HashMap[String, CString]
+  private implicit val atomZone: Zone = Zone.open()
+  private val atoms                   = new mutable.HashMap[String, CString]
 
   private def atom(s: String) =
     s match {
@@ -66,7 +69,7 @@ package object iup {
         }
     }
 
-  private def copyChild(child: Seq[Ihandle]): Ptr[lib.IhandlePtr] = {
+  private def copyChild(child: Seq[Handle]): Ptr[lib.IhandlePtr] = {
     val cs = alloc[lib.IhandlePtr]((child.length + 1).toUInt)
 
     for ((c, i) <- child.zipWithIndex)
@@ -80,7 +83,7 @@ package object iup {
 
   // the callback map also stores the a reference to the object for which the callback was set
   //   so that it doesn't necessarily have to be a value class
-  private val callbackMap = new mutable.HashMap[lib.IhandlePtr, (Ihandle, Ihandle => IupReturn)]
+  private val callbackMap = new mutable.HashMap[lib.IhandlePtr, (Handle, Handle => Return)]
 
   private def internalCallback(self: lib.IhandlePtr): CInt = {
     val (arg, func) = callbackMap(self)
@@ -92,10 +95,10 @@ package object iup {
     def x(height: Int): (Int, Int) = (width, height)
   }
 
-  implicit class Ihandle(val ih: lib.IhandlePtr) extends AnyVal with Dynamic {
+  implicit class Handle(val ih: lib.IhandlePtr) extends AnyVal with Dynamic {
 //    def selectDynamic(name: String): String = {}
 
-    def applyDynamicNamed(method: String)(attrs: (String, Any)*): Ihandle =
+    def applyDynamicNamed(method: String)(attrs: (String, Any)*): Handle =
       if (method == "apply") {
         attrs foreach { case (k, v) => updateDynamic(k)(v) }
         this
@@ -107,37 +110,37 @@ package object iup {
         case n: Int                    => lib.IupSetAttribute(ih, atom(name.toUpperCase), atom(n.toString))
         case (width: Int, height: Int) => lib.IupSetAttribute(ih, atom(name.toUpperCase), atom(s"${width}x$height"))
         case callback: Function1[_, _] =>
-          callbackMap(ih) = (this, callback.asInstanceOf[Ihandle => IupReturn])
+          callbackMap(ih) = (this, callback.asInstanceOf[Handle => Return])
           lib.IupSetCallback(ih, atom(name.toUpperCase), internalCallback _)
       }
     }
 
-    def IupShowXY(x: IupPosition, y: IupPosition): IupResult = lib.IupShowXY(ih, x.pos, y.pos)
+    def showXY(x: Position, y: Position): Result = lib.IupShowXY(ih, x.pos, y.pos)
   }
 
   // Main API
 
-  def IupOpen: IupResult = lib.IupOpen(null, null)
-  def IupClose(): Unit = {
+  def open: Result = lib.IupOpen(null, null)
+  def close(): Unit = {
     lib.IupClose()
     atomZone.close()
   }
-  def IupIsOpened: Boolean = lib.IupIsOpened
+  def isOpened: Boolean = lib.IupIsOpened
 
-  def IupMainLoop: IupResult = lib.IupMainLoop
+  def mainLoop: Result = lib.IupMainLoop
 
   // Elements
 
-  def IupVbox(child: Ihandle*): Ihandle = lib.IupVboxv(copyChild(child))
+  def vbox(child: Handle*): Handle = lib.IupVboxv(copyChild(child))
 
-  def IupButton(title: String, action: String): Ihandle = lib.IupButton(atom(title), atom(action))
-  def IupDialog(child: Ihandle): Ihandle                = lib.IupDialog(child.ih)
-  def IupLabel(title: String): Ihandle                  = lib.IupLabel(atom(title))
+  def button(title: String, action: String): Handle = lib.IupButton(atom(title), atom(action))
+  def dialog(child: Handle): Handle                 = lib.IupDialog(child.ih)
+  def label(title: String): Handle                  = lib.IupLabel(atom(title))
 
   // Utilities
 
   // Pre-defined dialogs
 
-  def IupMessage(title: String, msg: String): Unit = lib.IupMessage(atom(title), atom(msg))
+  def message(title: String, msg: String): Unit = lib.IupMessage(atom(title), atom(msg))
 
 }
