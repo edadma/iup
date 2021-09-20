15e6d95ff772e19bab89365bb4ab7699b84cabb8
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-20 09:51:22 -0400

    example 3.4

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -133,7 +133,103 @@
 //
 //}
 
-//### [3.3 Using Pre-defined Dialogs](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial3.html#Using_Pre_Dialogs)
+////### [3.3 Using Pre-defined Dialogs](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial3.html#Using_Pre_Dialogs)
+//
+//import java.nio.file.{Files, Paths}
+//import scala.util.{Failure, Success}
+//import io.github.edadma.iup
+//import io.github.edadma.iup.{Handle, Position, Return}
+//
+//object Main extends App {
+//
+//  def read_file(filename: String): String =
+//    util.Using(scala.io.Source.fromFile(filename))(_.mkString) match {
+//      case Failure(exception) =>
+//        iup.message("Error", exception.getMessage)
+//        null
+//      case Success(text) => text
+//    }
+//
+//  def write_file(filename: String, str: String): Unit =
+//    try {
+//      Files.write(Paths.get(filename), str.getBytes)
+//    } catch {
+//      case e: Exception => iup.message("Error", e.getMessage)
+//    }
+//
+//  val open_cb = (_: Handle) => {
+//    val filedlg = iup.fileDlg.set(dialogtype = "open", extfilter = "Text Files|*.txt|All Files|*.*|")
+//
+//    filedlg.popup(Position.CENTER, Position.CENTER)
+//
+//    if (filedlg.int.status != -1)
+//      read_file(filedlg.value) match {
+//        case null =>
+//        case str  => multitext.str.value = str
+//      }
+//
+//    filedlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val saveas_cb = (_: Handle) => {
+//    val filedlg = iup.fileDlg.set(dialogtype = "save", extfilter = "Text Files|*.txt|All Files|*.*|")
+//
+//    filedlg.popup(Position.CENTER, Position.CENTER)
+//
+//    if (filedlg.int.status != -1)
+//      write_file(filedlg.value, multitext.value)
+//
+//    filedlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val fond_cb = (_: Handle) => {
+//    val fontdlg = iup.fontDlg
+//    val font    = multitext.font
+//
+//    fontdlg.str.value = font
+//    fontdlg.popup(Position.CENTER, Position.CENTER)
+//
+//    if (fontdlg.int.status != -1)
+//      multitext.str.font = fontdlg.value
+//
+//    fontdlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val about_cb = (_: Handle) => {
+//    iup.message("About", "Simple Notepad\n\nOriginal authors:\n Gustavo Lyrio\n Antonio Scuri")
+//    Return.DEFAULT
+//  }
+//
+//  val exit_cb = (_: Handle) => Return.CLOSE
+//
+//  iup.open
+//
+//  val multitext       = iup.text(null)(multiline = "yes", expand = "yes")
+//  val item_open       = iup.item("Open...", null)(action = open_cb)
+//  val item_saveas     = iup.item("Save As...", null)(action = saveas_cb)
+//  val item_exit       = iup.item("Exit", null)(action = exit_cb)
+//  val item_font       = iup.item("Font...", null)(action = fond_cb)
+//  val item_about      = iup.item("About...", null)(action = about_cb)
+//  val file_menu       = iup.menu(item_open, item_saveas, iup.separator, item_exit)
+//  val format_menu     = iup.menu(item_font)
+//  val help_menu       = iup.menu(item_about)
+//  val sub_menu_file   = iup.submenu("File", file_menu)
+//  val sub_format_file = iup.submenu("Format", format_menu)
+//  val sub_menu_help   = iup.submenu("Help", help_menu)
+//  val menu            = iup.menu(sub_menu_file, sub_format_file, sub_menu_help)
+//  val vbox            = iup.vbox(multitext)
+//  val dlg             = iup.dialog(vbox)(menu = menu, title = "Simple Notepad", size = "QUARTERxQUARTER")
+//
+//  dlg.showXY(Position.CENTER, Position.CENTER)
+//  iup.mainLoop
+//  iup.close()
+//
+//}
+
+//### [3.4 Custom Dialogs](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial3.html#Custom_Dialogs)
 
 import java.nio.file.{Files, Paths}
 import scala.util.{Failure, Success}
@@ -141,6 +237,27 @@ import io.github.edadma.iup
 import io.github.edadma.iup.{Handle, Position, Return}
 
 object Main extends App {
+
+  def str_compare(l: String, idx: Int, r: String, casesensitive: Boolean): Boolean =
+    if (l.length - idx < r.length) false
+    else {
+      for (i <- r.indices) {
+        val equal = if (casesensitive) l(idx + i).toLower == r(i).toLower else l(idx + i) == r(i)
+
+        if (!equal)
+          return false
+      }
+
+      true
+    }
+
+  def str_find(str: String, str_to_find: String, casesensitive: Boolean): Int = {
+    for (i <- str.indices)
+      if (str_compare(str, i, str_to_find, casesensitive))
+        return i
+
+    -1
+  }
 
   def read_file(filename: String): String =
     util.Using(scala.io.Source.fromFile(filename))(_.mkString) match {
