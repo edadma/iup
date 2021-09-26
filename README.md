39743a77af3b415050428d0906c0c52e5143aa60
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-26 06:19:17 -0400

    readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ iup
 Overview
 --------
 
-The goal of this project is to provide easy-to-use Scala Native bindings for the IUP user interface library.
+The goal of this project is to provide easy-to-use Scala Native bindings for the IUP user interface library.  Ultimately, all functions will be covered, and all sample C applications in chapters 3 and 4 of the [Tutorial](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial.html) as well as in the chapter [7GUIs Implementation in IUP](https://www.tecgraf.puc-rio.br/iup/en/7gui/7gui.html) will be translated.
 
 The more "programmer friendly" part of this library is found in the `io.github.edadma.iup` package. That's the only
 package you need to import from, as seen in the example below. The other package in the library
@@ -20,17 +20,16 @@ Scala bindings library. Consequently, you never have to worry about memory alloc
 Usage
 -----
 
-To use this library, `iup-dev` needs to be installed:
+Two of the libraries (`iup` and `img`) have been built and placed in the `native-lib` folder.  To use them, include the following in your `build.sbt`:
 
-```shell
-sudo apt install iup-dev
+```sbt
+nativeLinkingOptions := Seq(s"-L${baseDirectory.value}/native-lib")
 ```
 
 Include the following in your `project/plugins.sbt`:
 
 ```sbt
 addSbtPlugin("com.codecommit" % "sbt-github-packages" % "0.5.2")
-
 ```
 
 Include the following in your `build.sbt`:
@@ -39,7 +38,6 @@ Include the following in your `build.sbt`:
 resolvers += Resolver.githubPackages("edadma")
 
 libraryDependencies += "io.github.edadma" %%% "iup" % "0.1.0"
-
 ```
 
 Use the following `import` statement in your code:
@@ -52,170 +50,37 @@ import io.github.edadma.iup._
 Examples
 --------
 
-The following examples are taken directly from the [YAML Specification](https://yaml.org/spec/1.2.1/) without any changes.
+The following examples are translated directly from chapter 2 of [Tutorial](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial2.html) without any changes.
 
-### Example 24
-
-This example is [Example 24](https://yaml.org/spec/1.2.1/#id2761292) of the YAML spec.
+### [2.1 Initialization](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial2.html#Initialization)
 
 ```scala
-import io.github.edadma.libyaml._
-
-import pprint._
+import io.github.edadma.iup
+import io.github.edadma.iup.Result
 
 object Main extends App {
 
-  val example_24 =
-    """
-      |%TAG ! tag:clarkevans.com,2002:
-      |--- !shape
-      |  # Use the ! handle for presenting
-      |  # tag:clarkevans.com,2002:circle
-      |- !circle
-      |  center: &ORIGIN {x: 73, y: 129}
-      |  radius: 7
-      |- !line
-      |  start: *ORIGIN
-      |  finish: { x: 89, y: 102 }
-      |- !label
-      |  start: *ORIGIN
-      |  color: 0xFFEEBB
-      |  text: Pretty vector drawing.
-      |""".stripMargin
+  if (iup.open == Result.ERROR) {
+    println("Error opening window")
+    sys.exit(1)
+  }
 
-  pprintln(parseFromString(example_24))
+  iup.message("Hello World 1", "Hello world from IUP.")
+  iup.close()
 
 }
 ```
 
-Output:
-
-```scala
-YAMLStream(
-  documents = List(
-    YAMLDocument(
-      document = YAMLTaggedSequence(
-        tag = "tag:clarkevans.com,2002:shape",
-        elems = List(
-          YAMLTaggedMapping(
-            tag = "tag:clarkevans.com,2002:circle",
-            pairs = List(
-              YAMLPair(
-                key = YAMLString(v = "center"),
-                value = YAMLMapping(
-                  pairs = List(
-                    YAMLPair(key = YAMLString(v = "x"), value = YAMLInteger(v = 73)),
-                    YAMLPair(key = YAMLString(v = "y"), value = YAMLInteger(v = 129))
-                  )
-                )
-              ),
-              YAMLPair(key = YAMLString(v = "radius"), value = YAMLInteger(v = 7))
-            )
-          ),
-          YAMLTaggedMapping(
-            tag = "tag:clarkevans.com,2002:line",
-            pairs = List(
-              YAMLPair(
-                key = YAMLString(v = "start"),
-                value = YAMLMapping(
-                  pairs = List(
-                    YAMLPair(key = YAMLString(v = "x"), value = YAMLInteger(v = 73)),
-                    YAMLPair(key = YAMLString(v = "y"), value = YAMLInteger(v = 129))
-                  )
-                )
-              ),
-              YAMLPair(
-                key = YAMLString(v = "finish"),
-                value = YAMLMapping(
-                  pairs = List(
-                    YAMLPair(key = YAMLString(v = "x"), value = YAMLInteger(v = 89)),
-                    YAMLPair(key = YAMLString(v = "y"), value = YAMLInteger(v = 102))
-                  )
-                )
-              )
-            )
-          ),
-          YAMLTaggedMapping(
-            tag = "tag:clarkevans.com,2002:label",
-            pairs = List(
-              YAMLPair(
-                key = YAMLString(v = "start"),
-                value = YAMLMapping(
-                  pairs = List(
-                    YAMLPair(key = YAMLString(v = "x"), value = YAMLInteger(v = 73)),
-                    YAMLPair(key = YAMLString(v = "y"), value = YAMLInteger(v = 129))
-                  )
-                )
-              ),
-              YAMLPair(key = YAMLString(v = "color"), value = YAMLInteger(v = 16772795)),
-              YAMLPair(
-                key = YAMLString(v = "text"),
-                value = YAMLString(v = "Pretty vector drawing.")
-              )
-            )
-          )
-        )
-      )
-    )
-  )
-)
-```
-
-### Example 26
-
-This example is [Example 26](https://yaml.org/spec/1.2.1/#id2761292) of the YAML spec.
-
-```scala
-import io.github.edadma.libyaml._
-
-import pprint._
-
-object Main extends App {
-
-  val example_26 =
-    """
-        |# Ordered maps are represented as
-        |# A sequence of mappings, with
-        |# each mapping having one key
-        |--- !!omap
-        |- Mark McGwire: 65
-        |- Sammy Sosa: 63
-        |- Ken Griffy: 58
-        |""".stripMargin
-
-  pprintln(parseFromString(example_26))
-  pprintln(constructFromString(example_26))
-
-}
-
-```
-
-Output:
-
-```scala
-YAMLStream(
-  documents = List(
-    YAMLDocument(
-      document = YAMLOrderedMapping(
-        pairs = List(
-          YAMLPair(key = YAMLString(v = "Mark McGwire"), value = YAMLInteger(v = 65)),
-          YAMLPair(key = YAMLString(v = "Sammy Sosa"), value = YAMLInteger(v = 63)),
-          YAMLPair(key = YAMLString(v = "Ken Griffy"), value = YAMLInteger(v = 58))
-        )
-      )
-    )
-  )
-)
-List(VectorMap("Mark McGwire" -> 65, "Sammy Sosa" -> 63, "Ken Griffy" -> 58))
-```
+Screenshot:
 
 Documentation
 -------------
 
-API documentation is forthcoming, however documentation for the *Libyaml* C library is
-found [here](https://pyyaml.org/wiki/PyYAMLDocumentation).
+API documentation is forthcoming, however documentation for the *IUP* C library is
+found [here](https://www.tecgraf.puc-rio.br/iup/).  Build instructions of the C libraries are [here](https://www.tecgraf.puc-rio.br/iup/en/guide.html#buildlib).
+
 
 License
 -------
 
-[ISC](https://github.com/edadma/iup/blob/main/LICENSE)
+This project is licensed under the [ISC](https://github.com/edadma/iup/blob/main/LICENSE).  The IUP C libraries (included in the `native-lib` folder) are under the [Tecgraf Library License](https://www.tecgraf.puc-rio.br/iup/en/copyright.html).
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,26 @@
-//object Main extends App {
-//
-//  if (IupOpen == IUP_ERROR) {
-//    println("Error opening window")
-//    sys.exit(1)
-//  }
-//
-//  IupMessage("Hello World 1", "Hello world from IUP.")
-//
-//  IupClose()
-//
-//}
+//### [2.1 Initialization](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial2.html#Initialization)
 
+import io.github.edadma.iup
+import io.github.edadma.iup.Result
+
+object Main extends App {
+
+  if (iup.open == Result.ERROR) {
+    println("Error opening window")
+    sys.exit(1)
+  }
+
+  iup.message("Hello World 1", "Hello world from IUP.")
+  iup.close()
+
+}
+
+//### [2.1 Initialization](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial2.html#Initialization)
+
+//import io.github.edadma.iup
+//import io.github.edadma.iup.Implicits._
+//import io.github.edadma.iup.{Handle, Position, Return, Result}
+//
 //object Main extends App {
 //
 //  if (IupOpen == IUP_ERROR) {
@@ -230,177 +240,177 @@
 //}
 
 //### [3.4 Custom Dialogs](https://www.tecgraf.puc-rio.br/iup/en/tutorial/tutorial3.html#Custom_Dialogs)
-
-import java.nio.file.{Files, Paths}
-import scala.util.{Failure, Success}
-import io.github.edadma.iup
-import io.github.edadma.iup.Implicits._
-import io.github.edadma.iup.{Handle, Position, Return}
-
-object Main extends App {
-
-  def str_compare(l: String, idx: Int, r: String, casesensitive: Boolean): Boolean =
-    if (l.length - idx < r.length) false
-    else {
-      for (i <- r.indices) {
-        val equal = if (casesensitive) l(idx + i).toLower == r(i).toLower else l(idx + i) == r(i)
-
-        if (!equal)
-          return false
-      }
-
-      true
-    }
-
-  def str_find(str: String, str_to_find: String, casesensitive: Boolean): Int = {
-    for (i <- str.indices)
-      if (str_compare(str, i, str_to_find, casesensitive))
-        return i
-
-    -1
-  }
-
-  def read_file(filename: String): String =
-    util.Using(scala.io.Source.fromFile(filename))(_.mkString) match {
-      case Failure(exception) =>
-        iup.message("Error", exception.getMessage)
-        null
-      case Success(text) => text
-    }
-
-  def write_file(filename: String, str: String): Unit =
-    try {
-      Files.write(Paths.get(filename), str.getBytes)
-    } catch {
-      case e: Exception => iup.message("Error", e.getMessage)
-    }
-
-  val item_open_action_cb = (item_open: Handle) => {
-    val filedlg =
-      iup.fileDlg.set(dialogtype = "open", extfilter = "Text Files|*.txt|All Files|*.*|", parentdialog = item_open.getDialog)
-
-    filedlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
-
-    if (filedlg.int.status != -1)
-      read_file(filedlg.value) match {
-        case null =>
-        case str  => multitext.str.value = str
-      }
-
-    filedlg.destroy()
-    Return.DEFAULT
-  }
-
-  val item_saveas_action_cb = (item_saveas: Handle) => {
-    val filedlg =
-      iup.fileDlg.set(dialogtype = "save", extfilter = "Text Files|*.txt|All Files|*.*|", parentdialog = item_saveas.getDialog)
-
-    filedlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
-
-    if (filedlg.int.status != -1)
-      write_file(filedlg.value, multitext.value)
-
-    filedlg.destroy()
-    Return.DEFAULT
-  }
-
-  val item_exit_action_cb = (_: Handle) => Return.CLOSE
-
-  val goto_ok_action_cb = (bt_ok: Handle) => {
-    val line_count = bt_ok.int.text_linecount
-    val txt        = bt_ok.getDialogChild("LINE_TEXT")
-    val line       = txt.int.value
-
-    if (line < 1 || line > line_count) {
-      iup.message("Error", "Invalid line number.")
-      Return.DEFAULT
-    } else {
-      bt_ok.getDialog.status = 1
-      Return.CLOSE
-    }
-  }
-
-  val goto_cancel_action_cb = (bt_ok: Handle) => {
-    bt_ok.getDialog.status = 0
-    Return.CLOSE
-  }
-
-  val item_goto_action_cb = (item_goto: Handle) => {
-    val multitext  = item_goto.getDialogChild("multitext")
-    val line_count = multitext.int.linecount
-    val lbl        = iup.label(null)
-
-    lbl.str.title = s"Line Number [1-$line_count]:"
-
-    val txt       = iup.text(null)(mask = iup.MASK_UINT, name = "line_text", visiblecolunms = 20)
-    val bt_ok     = iup.button("OK", null)(text_linecount = line_count, padding = 10 x 2, action = goto_ok_action_cb)
-    val bt_cancel = iup.button("Cancel", null)(action = goto_cancel_action_cb, padding = 10 x 2)
-    val box =
-      iup.vbox(lbl, txt, iup.hbox(iup.fill, bt_ok, bt_cancel) setAttributes "NORMALIZESIZE=HORIZONTAL")(margin = 10 x 10, gap = 5)
-    val dlg = iup.dialog(box)(title = "Go To Line",
-                              dialogframe = "yes",
-                              defaultenter = bt_ok,
-                              defaultesc = bt_cancel,
-                              parentdialog = item_goto.getDialog)
-
-    dlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
-    multitext.setFocus
-
-    if (dlg.int.status == 1) {
-      val line = txt.int.value
-      val pos  = multitext.textConvertLinColToPos(line, 0)
-
-      multitext.int.caretpos = pos
-      multitext.int.scrolltopos = pos
-    }
-
-    dlg.destroy()
-    Return.DEFAULT
-  }
-
-  val find_next_action_cb = (bt_next: Handle) => {
-    val multitext = bt_next.handle.multitext
-  }
-
-  val fond_cb = (_: Handle) => {
-    val fontdlg = iup.fontDlg
-    val font    = multitext.font
-
-    fontdlg.str.value = font
-    fontdlg.popup(Position.CENTER, Position.CENTER)
-
-    if (fontdlg.int.status != -1)
-      multitext.str.font = fontdlg.value
-
-    fontdlg.destroy()
-    Return.DEFAULT
-  }
-
-  val about_cb = (_: Handle) => {
-    iup.message("About", "Simple Notepad\n\nOriginal authors:\n Gustavo Lyrio\n Antonio Scuri")
-    Return.DEFAULT
-  }
-
-  iup.open
-
-  val multitext       = iup.text(null)(multiline = "yes", expand = "yes")
-  val item_open       = iup.item("Open...", null)(action = item_open_action_cb)
-  val item_saveas     = iup.item("Save As...", null)(action = item_saveas_action_cb)
-  val item_exit       = iup.item("Exit", null)(action = item_exit_action_cb)
-  val item_font       = iup.item("Font...", null)(action = fond_cb)
-  val item_about      = iup.item("About...", null)(action = about_cb)
-  val file_menu       = iup.menu(item_open, item_saveas, iup.separator, item_exit)
-  val format_menu     = iup.menu(item_font)
-  val help_menu       = iup.menu(item_about)
-  val sub_menu_file   = iup.submenu("File", file_menu)
-  val sub_format_file = iup.submenu("Format", format_menu)
-  val sub_menu_help   = iup.submenu("Help", help_menu)
-  val menu            = iup.menu(sub_menu_file, sub_format_file, sub_menu_help)
-  val vbox            = iup.vbox(multitext)
-  val dlg             = iup.dialog(vbox)(menu = menu, title = "Simple Notepad", size = "QUARTERxQUARTER")
-
-  dlg.showXY(Position.CENTER, Position.CENTER)
-  iup.mainLoop
-  iup.close()
-
-}
+//
+//import java.nio.file.{Files, Paths}
+//import scala.util.{Failure, Success}
+//import io.github.edadma.iup
+//import io.github.edadma.iup.Implicits._
+//import io.github.edadma.iup.{Handle, Position, Return}
+//
+//object Main extends App {
+//
+//  def str_compare(l: String, idx: Int, r: String, casesensitive: Boolean): Boolean =
+//    if (l.length - idx < r.length) false
+//    else {
+//      for (i <- r.indices) {
+//        val equal = if (casesensitive) l(idx + i).toLower == r(i).toLower else l(idx + i) == r(i)
+//
+//        if (!equal)
+//          return false
+//      }
+//
+//      true
+//    }
+//
+//  def str_find(str: String, str_to_find: String, casesensitive: Boolean): Int = {
+//    for (i <- str.indices)
+//      if (str_compare(str, i, str_to_find, casesensitive))
+//        return i
+//
+//    -1
+//  }
+//
+//  def read_file(filename: String): String =
+//    util.Using(scala.io.Source.fromFile(filename))(_.mkString) match {
+//      case Failure(exception) =>
+//        iup.message("Error", exception.getMessage)
+//        null
+//      case Success(text) => text
+//    }
+//
+//  def write_file(filename: String, str: String): Unit =
+//    try {
+//      Files.write(Paths.get(filename), str.getBytes)
+//    } catch {
+//      case e: Exception => iup.message("Error", e.getMessage)
+//    }
+//
+//  val item_open_action_cb = (item_open: Handle) => {
+//    val filedlg =
+//      iup.fileDlg.set(dialogtype = "open", extfilter = "Text Files|*.txt|All Files|*.*|", parentdialog = item_open.getDialog)
+//
+//    filedlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
+//
+//    if (filedlg.int.status != -1)
+//      read_file(filedlg.value) match {
+//        case null =>
+//        case str  => multitext.str.value = str
+//      }
+//
+//    filedlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val item_saveas_action_cb = (item_saveas: Handle) => {
+//    val filedlg =
+//      iup.fileDlg.set(dialogtype = "save", extfilter = "Text Files|*.txt|All Files|*.*|", parentdialog = item_saveas.getDialog)
+//
+//    filedlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
+//
+//    if (filedlg.int.status != -1)
+//      write_file(filedlg.value, multitext.value)
+//
+//    filedlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val item_exit_action_cb = (_: Handle) => Return.CLOSE
+//
+//  val goto_ok_action_cb = (bt_ok: Handle) => {
+//    val line_count = bt_ok.int.text_linecount
+//    val txt        = bt_ok.getDialogChild("LINE_TEXT")
+//    val line       = txt.int.value
+//
+//    if (line < 1 || line > line_count) {
+//      iup.message("Error", "Invalid line number.")
+//      Return.DEFAULT
+//    } else {
+//      bt_ok.getDialog.status = 1
+//      Return.CLOSE
+//    }
+//  }
+//
+//  val goto_cancel_action_cb = (bt_ok: Handle) => {
+//    bt_ok.getDialog.status = 0
+//    Return.CLOSE
+//  }
+//
+//  val item_goto_action_cb = (item_goto: Handle) => {
+//    val multitext  = item_goto.getDialogChild("multitext")
+//    val line_count = multitext.int.linecount
+//    val lbl        = iup.label(null)
+//
+//    lbl.str.title = s"Line Number [1-$line_count]:"
+//
+//    val txt       = iup.text(null)(mask = iup.MASK_UINT, name = "line_text", visiblecolunms = 20)
+//    val bt_ok     = iup.button("OK", null)(text_linecount = line_count, padding = 10 x 2, action = goto_ok_action_cb)
+//    val bt_cancel = iup.button("Cancel", null)(action = goto_cancel_action_cb, padding = 10 x 2)
+//    val box =
+//      iup.vbox(lbl, txt, iup.hbox(iup.fill, bt_ok, bt_cancel) setAttributes "NORMALIZESIZE=HORIZONTAL")(margin = 10 x 10, gap = 5)
+//    val dlg = iup.dialog(box)(title = "Go To Line",
+//                              dialogframe = "yes",
+//                              defaultenter = bt_ok,
+//                              defaultesc = bt_cancel,
+//                              parentdialog = item_goto.getDialog)
+//
+//    dlg.popup(Position.CENTERPARENT, Position.CENTERPARENT)
+//    multitext.setFocus
+//
+//    if (dlg.int.status == 1) {
+//      val line = txt.int.value
+//      val pos  = multitext.textConvertLinColToPos(line, 0)
+//
+//      multitext.int.caretpos = pos
+//      multitext.int.scrolltopos = pos
+//    }
+//
+//    dlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val find_next_action_cb = (bt_next: Handle) => {
+//    val multitext = bt_next.handle.multitext
+//  }
+//
+//  val fond_cb = (_: Handle) => {
+//    val fontdlg = iup.fontDlg
+//    val font    = multitext.font
+//
+//    fontdlg.str.value = font
+//    fontdlg.popup(Position.CENTER, Position.CENTER)
+//
+//    if (fontdlg.int.status != -1)
+//      multitext.str.font = fontdlg.value
+//
+//    fontdlg.destroy()
+//    Return.DEFAULT
+//  }
+//
+//  val about_cb = (_: Handle) => {
+//    iup.message("About", "Simple Notepad\n\nOriginal authors:\n Gustavo Lyrio\n Antonio Scuri")
+//    Return.DEFAULT
+//  }
+//
+//  iup.open
+//
+//  val multitext       = iup.text(null)(multiline = "yes", expand = "yes")
+//  val item_open       = iup.item("Open...", null)(action = item_open_action_cb)
+//  val item_saveas     = iup.item("Save As...", null)(action = item_saveas_action_cb)
+//  val item_exit       = iup.item("Exit", null)(action = item_exit_action_cb)
+//  val item_font       = iup.item("Font...", null)(action = fond_cb)
+//  val item_about      = iup.item("About...", null)(action = about_cb)
+//  val file_menu       = iup.menu(item_open, item_saveas, iup.separator, item_exit)
+//  val format_menu     = iup.menu(item_font)
+//  val help_menu       = iup.menu(item_about)
+//  val sub_menu_file   = iup.submenu("File", file_menu)
+//  val sub_format_file = iup.submenu("Format", format_menu)
+//  val sub_menu_help   = iup.submenu("Help", help_menu)
+//  val menu            = iup.menu(sub_menu_file, sub_format_file, sub_menu_help)
+//  val vbox            = iup.vbox(multitext)
+//  val dlg             = iup.dialog(vbox)(menu = menu, title = "Simple Notepad", size = "QUARTERxQUARTER")
+//
+//  dlg.showXY(Position.CENTER, Position.CENTER)
+//  iup.mainLoop
+//  iup.close()
+//
+//}
